c47f4bdd2e9a2519d244fe80f034b177dc804909
Ivan Junior <ivanjfbr@gmail.com>
2024-10-04 00:40:45 -0300

    Add phone number into user schema

--- orby-api/src/routes/teste.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const transactionRouter = Router();
-
-transactionRouter.get("/teste", (req, res) => {
-  res.send("Hello World!");
-});
-
-export default transactionRouter;
--- orby-api/src/schemas/validation/CreateUser.js
+++ orby-api/src/schemas/validation/CreateUser.js
@@ -3,6 +3,7 @@ import Joi from "joi";
 export const CreateUser = Joi.object({
   name: Joi.string().required().min(3),
   email: Joi.string().email().required().min(3),
+  phoneNumber: Joi.string().required().min(12),
   password: Joi.string().required().min(3),
   createdAt: Joi.string(),
 });
