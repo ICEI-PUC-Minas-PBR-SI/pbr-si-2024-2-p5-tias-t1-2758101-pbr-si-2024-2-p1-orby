90bfb4de4318dfbe532a7665cf7de9cdeea11319
Ivan Junior <36963829+IvanJFBR@users.noreply.github.com>
2024-12-02 13:50:48 -0300

    Tela de noticias (#19)

--- orby-app/src/pages/news/news.js
+++ orby-app/src/pages/news/news.js
@@ -1,12 +1,58 @@
-import { StyleSheet, View } from "react-native";
 import React from "react";
-import OrbyLogo from "../../components/orby_logo/OrbyLogo";
+import { View, Text, Image, ScrollView } from "react-native";
+import styles from "./news.style";
 
 export function News({ navigation }) {
   return (
-    <View style={styles.container}>
-      <OrbyLogo title="Em desenvolvimento..." />
-    </View>
+    <ScrollView style={styles.container}>
+      {/* Logo do aplicativo */}
+      <Image
+        source={require("../../assets/orby_logo.png")}
+        style={styles.logo}
+      />
+
+      {/* Título da página */}
+      <Text style={styles.header}>Notícias Recentes</Text>
+
+      <View style={styles.card}>
+        <Image
+          source={require("../../assets/estoque_sangue.png")}
+          style={styles.sponsorImage1}
+        />
+      </View>
+
+      <View style={styles.card}>
+        <Image
+          source={require("../../assets/noticias1.png")}
+          style={styles.sponsorImage}
+        />
+        <Text style={styles.sponsorTitle}>
+          Hemocentro de Belo Horizonte recebe a quarta recertificação
+          internacional de qualidade dos serviços
+        </Text>
+        <Text style={styles.sponsorDescription}>
+          “Tenho muito orgulho de dizer que a Hemominas tem o melhor sangue do
+          Brasil. Além disso, pelas minhas viagens como presidente do Conselho
+          Nacional de Secretários de Saúde...
+        </Text>
+      </View>
+
+      <View style={styles.card}>
+        <Image
+          source={require("../../assets/noticias2.jpg")}
+          style={styles.sponsorImage}
+        />
+        <Text style={styles.sponsorTitle}>
+          Hemominas faz grande participação no Congresso Brasileiro de
+          Hematologia, Hemoterapia e Terapia Celular – Hemo 2024{" "}
+        </Text>
+        <Text style={styles.sponsorDescription}>
+          “Equipes de toda a Rede da Fundação Hemominas participaram, de 23 a 26
+          de outubro, do Congresso Brasileiro de Hematologia, Hemoterapia e
+          Terapia Celular – Hemo 2024
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
 
